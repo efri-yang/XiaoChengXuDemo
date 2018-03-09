d3e450a14f6adbd36c8b8f8c8c70566516cca200
efri-yang <948061564@qq.com>
2018-03-09 13:07:25 +0800

    c

--- AOnlineProject/pages/user/user.js
+++ AOnlineProject/pages/user/user.js
@@ -6,7 +6,7 @@ Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('view.open-type.getUserInfo'),
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -15,14 +15,21 @@ Page({
         hasUserInfo: true
       })
     } else if (this.data.canIUse) {
-      console.dir("asdfasdfasdf");
+
     }
+  },
+  hlogin:function(){
+     wx.login({
+        success:res=>{
+            console.dir("xxxxx");
+        }
+     })
   },
   //点击获取头像的时候回去执行这个函数（不管是允许还是拒绝，允许就会返回用户微信的基本信息，拒绝就是undefinded）
   getUserInfo: function (e) {
     if (!!e.detail.userInfo){
       //如果用户选择允许
-      console.dir("允许用户用户信息");
+
     }else{
       //如果用户选择拒绝
       console.dir("拒绝用户用户信息");
