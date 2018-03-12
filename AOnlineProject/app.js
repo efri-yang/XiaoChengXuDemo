c2df9ab8393e212dfe28a782be24aafd6657f7e7
efri-yang <948061564@qq.com>
2018-03-12 09:49:20 +0800

    c

--- AOnlineProject/app.js
+++ AOnlineProject/app.js
@@ -1,30 +1,33 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    wx.login({
-        success:res=>{
-            wx.request({
-              url: this.globalData.server+"/login",
-              data:{
-                code:res.code
-              }
-            })
-        }
-    })
-  },
-  globalData: {
-    userInfo: null,
-    loginCode: null,
-    encryptedData: null,
-    iv: null,
-    server: 'https://wnworld.com/api/xiaochengxu'
-  }
-  
+    onLaunch: function() {
+        // 展示本地存储能力
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+        wx.login({
+            success: res => {
+                wx.request({
+                    url: this.globalData.server + "/login",
+                    data: {
+                        code: res.code
+                    },
+                    success: function(res) {
+                      
+                    }
+                })
+            }
+        })
+    },
+    globalData: {
+        userInfo: null,
+        loginCode: null,
+        encryptedData: null,
+        iv: null,
+        server: 'https://wnworld.com/api/xiaochengxu'
+    }
 
- 
- 
+
+
+
 })
