b6ac15770f304264a2665daa35d89f1394bef1c8
efri-yang <948061564@qq.com>
2018-03-12 09:45:40 +0800

    c

--- AOnlineProject/app.js
+++ AOnlineProject/app.js
@@ -5,6 +5,16 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    wx.login({
+        success:res=>{
+            wx.request({
+              url: this.globalData.server+"/login",
+              data:{
+                code:res.code
+              }
+            })
+        }
+    })
   },
   globalData: {
     userInfo: null,
@@ -12,40 +22,6 @@ App({
     encryptedData: null,
     iv: null,
     server: 'https://wnworld.com/api/xiaochengxu'
-  },
-  getUserInfo: function (cb) {
-    var that = this;
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      wx.getSetting({
-        success: res1 => {
-          if (res1.authSetting['scope.userInfo']) {
-            that.globalData.userInfo = res1.userInfo;
-            that.globalData.iv = res1.iv;
-            that.globalData.encryptedData = res1.encryptedData;
-            typeof cb == "function" && cb(that.globalData.userInfo)
-          } else {
-            wx.getUserInfo({
-              success: res2 => {
-                if (res2)
-                that.globalData.userInfo = res2.userInfo
-                that.globalData.iv = res1.iv;
-                that.globalData.encryptedData = res1.encryptedData;
-                typeof cb == "function" && cb(that.globalData.userInfo)
-              }
-            })
-          }
-        }
-      })
-    }
-  },
-
-  _getUserInfo: function (e){
-    if (!!e.detail.userInfo) {
-      //用户同意
-        this.globalData.userInfo = e.detail.userInfo;
-    }
   }
   
 
